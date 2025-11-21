9008edf5dbe9a1714e67be8bae8f1e261073372a
lrexhepi <lrexhepi@uni-koeln.de>
2025-11-21 15:23:59 +0100

    Einzige Usage von OGridBagConstraints zu normalen GridBagConstraints umgewandelt

--- src/main/java/de/uzk/gui/tabs/TabNavigate.java
+++ src/main/java/de/uzk/gui/tabs/TabNavigate.java
@@ -2,7 +2,6 @@ package de.uzk.gui.tabs;
 
 import de.uzk.config.Config;
 import de.uzk.gui.Gui;
-import de.uzk.gui.OGridBagConstraints;
 import de.uzk.gui.observer.ObserverContainer;
 import de.uzk.image.Axis;
 import de.uzk.utils.ComponentUtils;
@@ -16,8 +15,6 @@ import static de.uzk.config.LanguageHandler.getWord;
 // TODO: Überarbeite Klasse
 public class TabNavigate extends ObserverContainer<JPanel> {
     // Gui Elemente
-    private JSlider timeSlider;
-    private JSlider levelSlider;
     private JSpinner timeUnitSpinner;
     private JSpinner levelUnitSpinner;
 
@@ -28,16 +25,17 @@ public class TabNavigate extends ObserverContainer<JPanel> {
 
     private void init() {
         this.container.setLayout(new GridBagLayout());
-
         // GridBagConstraints
-        OGridBagConstraints gbc = new OGridBagConstraints(new Insets(0, 0, 5, 15), GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL);
+        GridBagConstraints gbc = ComponentUtils.createGridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(0, 0, 5, 15);
 
         // imageLabel
         JLabel imageLabel = new JLabel(getWord("menu.nav.time.layer") + ":");
         this.container.add(imageLabel, gbc);
 
         // gbc
-        gbc.setPosAndInsets(0, 1, 0, 0, 0, 15);
+        gbc.gridy++;
 
         // timeSpinner
         SpinnerNumberModel timeSpinnerModel = new SpinnerNumberModel(30, Config.MIN_TIME_UNIT, Config.MAX_TIME_UNIT, 0.1);
@@ -45,69 +43,49 @@ public class TabNavigate extends ObserverContainer<JPanel> {
         this.container.add(timeUnitSpinner, gbc);
 
         // gbc
-        gbc.setPosAndInsets(1, 1, 0, 0, 0, 0);
+        gbc.gridx++;
         gbc.weightx = 1;
 
         // imageUnitLabel
+        gbc.anchor = GridBagConstraints.CENTER;
         JLabel imageUnitLabel = new JLabel(getWord("menu.nav.time.unit"));
         this.container.add(imageUnitLabel, gbc);
 
         // gbc
-        gbc.setPosAndInsets(1, 2, 10, 0, 0, 0);
-
-        // timeSlider
-        this.timeSlider = getSlider(Axis.TIME);
-        // TODO: Diese Slider tun zur Zeit nichts
-        // this.container.add(this.timeSlider, gbc);
-
-        // gbc
-        gbc.setPosAndInsets(0, 2, 15, 0, 5, 15);
+        gbc.gridy++;
+        gbc.gridx--;
         gbc.weightx = 0;
-
+        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
         // levelLabel
         JLabel levelLabel = new JLabel(getWord("menu.nav.level.layer") + ":");
         this.container.add(levelLabel, gbc);
 
         // gbc
-        gbc.setPosAndInsets(0, 3, 0, 0, 0, 15);
-
+        gbc.gridy++;
+        gbc.gridheight = 200;
         // timeSpinner
         SpinnerNumberModel levelSpinnerModel = new SpinnerNumberModel(1, Config.MIN_LEVEL_UNIT, Config.MAX_LEVEL_UNIT, 0.1);
         this.levelUnitSpinner = getUnitSpinner(levelSpinnerModel, Axis.LEVEL);
         this.container.add(levelUnitSpinner, gbc);
 
         // gbc
-        gbc.setPosAndInsets(1, 3, 0, 0, 0, 0);
+        gbc.gridx++;
         gbc.weightx = 1;
 
         // timeUnitLabel
+        gbc.anchor = GridBagConstraints.CENTER;
         JLabel levelUnitLabel = new JLabel(getWord("menu.nav.level.unit"));
         this.container.add(levelUnitLabel, gbc);
 
-        // gbc
-        gbc.setPosAndInsets(1, 5, 10, 0, 0, 0);
-        gbc.weighty = 1;
-        gbc.anchor = GridBagConstraints.NORTHWEST;
 
-        // levelSlider
-        this.levelSlider = getSlider(Axis.LEVEL);
-        // TODO: Diese Slider tun zur Zeit nichts
-        // this.container.add(this.levelSlider, gbc);
     }
+
 
     public JSpinner getUnitSpinner(SpinnerNumberModel spinnerModel, Axis axis) {
         JSpinner spinner = new JSpinner(spinnerModel);
 
         JSpinner.NumberEditor editor = new JSpinner.NumberEditor(spinner, "0.0");
         spinner.setEditor(editor);
-
-        Number number = switch (axis) {
-            case TIME -> workspace.getConfig().getTimeUnit();
-            case LEVEL -> workspace.getConfig().getLevelUnit();
-        };
-//        if (GuiUtils.valueFitsInRange(number, spinnerModel)) spinner.setValue(number);
-//        else updateUnitValue(spinner, axis);
-
 
         spinner.addChangeListener(e -> {
             if (spinner.isEnabled()) {
@@ -125,43 +103,9 @@ public class TabNavigate extends ObserverContainer<JPanel> {
         }
     }
 
-    private JSlider getSlider(Axis axis) {
-        JSlider slider = new JSlider(SwingConstants.HORIZONTAL);
-
-        slider.addChangeListener(e -> {
-            if (slider.isEnabled()) {
-                int value = slider.getValue();
-                boolean isAdjusting = slider.getValueIsAdjusting();
-                update(slider, axis, value, isAdjusting);
-            }
-        });
-        slider.setPaintLabels(true);
-        slider.setSnapToTicks(true);
-        return slider;
-    }
-
-    private void update(JSlider slider, Axis axis, int newValue, boolean isAdjusting) {
-
-
-        int oldValue = axis == Axis.TIME ? workspace.getTime() : workspace.getLevel();
-        if (oldValue == newValue) return;
-
-        if (!isAdjusting) {
-            // sets the slider to the oldValue
-            updateSliderValueSecretly(slider, oldValue);
-            return;
-        }
-
-//        if (axis == Axis.TIME) imageFileHandler.setTime(newValue);
-//        else imageFileHandler.setLevel(newValue);
-        gui.update(axis);
-    }
-
     @Override
     public void toggleOn() {
         ComponentUtils.setEnabled(this.container, true);
-        updateSliderValuesSecretly(timeSlider, workspace.getTime(), workspace.getMaxTime());
-        updateSliderValuesSecretly(levelSlider, workspace.getLevel(), workspace.getMaxLevel());
         this.timeUnitSpinner.setValue(workspace.getConfig().getTimeUnit());
         this.levelUnitSpinner.setValue(workspace.getConfig().getLevelUnit());
     }
@@ -169,34 +113,9 @@ public class TabNavigate extends ObserverContainer<JPanel> {
     @Override
     public void toggleOff() {
         ComponentUtils.setEnabled(this.container, false);
-        updateSliderValuesSecretly(timeSlider, 0, 0);
-        updateSliderValuesSecretly(levelSlider, 0, 0);
         this.timeUnitSpinner.setValue(workspace.getConfig().getTimeUnit());
         this.levelUnitSpinner.setValue(workspace.getConfig().getLevelUnit());
     }
 
-    @Override
-    public void update(Axis axis) {
-        if (axis == Axis.TIME) {
-            // sets only a new value to timeSlider if the slider is not moving
-            if (!timeSlider.getValueIsAdjusting()) updateSliderValueSecretly(timeSlider, workspace.getTime());
-        } else {
-            // sets only a new value to levelSlider if the slider is not moving
-            if (!levelSlider.getValueIsAdjusting()) updateSliderValueSecretly(levelSlider, workspace.getLevel());
-        }
-    }
 
-    private void updateSliderValueSecretly(JSlider slider, int value) {
-//        GuiUtils.updateSecretly(slider, () -> slider.setValue(value));
-    }
-
-    private void updateSliderValuesSecretly(JSlider slider, int value, int max) {
-//        GuiUtils.updateSecretly(slider, () -> {
-//            slider.setMaximum(max);
-//            slider.setMinimum(0);
-//            slider.setValue(value);
-//            slider.setMinorTickSpacing(1);
-//            slider.setMajorTickSpacing(max);
-//        });
-    }
 }
