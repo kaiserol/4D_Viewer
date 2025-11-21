30e872d56efa577d34a9caf605c8341c0c355c03
lrexhepi <lrexhepi@uni-koeln.de>
2025-11-21 11:49:58 +0100

    Unbenutzten MarkerPreview entfernt

--- src/main/java/de/uzk/gui/marker/MarkerEditor.java
+++ src/main/java/de/uzk/gui/marker/MarkerEditor.java
@@ -1,10 +1,8 @@
 package de.uzk.gui.marker;
 
-import de.uzk.gui.OGridBagConstraints;
 import de.uzk.gui.UIEnvironment;
 import de.uzk.gui.dialogs.DialogColorChooser;
 import de.uzk.image.ImageFile;
-import de.uzk.io.ImageLoader;
 import de.uzk.markers.Marker;
 import de.uzk.utils.DateTimeUtils;
 
@@ -14,15 +12,12 @@ import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.function.Consumer;
-import java.util.function.IntConsumer;
-import java.util.function.IntSupplier;
 
 import static de.uzk.Main.workspace;
 import static de.uzk.config.LanguageHandler.getWord;
 
 public class MarkerEditor extends Container {
     private final Marker marker;
-    private final MarkerPreview preview;
     private final java.util.List<Runnable> onUpdate = new ArrayList<>();
     private final DialogColorChooser dialogColorChooser;
 
@@ -32,8 +27,7 @@ public class MarkerEditor extends Container {
 
     public MarkerEditor(ImageFile onto, Marker marker) {
         this.marker = marker;
-        this.preview = new MarkerPreview(ImageLoader.loadImage(onto.getFilePath(), true), marker, this);
-        this.dialogColorChooser = new DialogColorChooser(null);
+         this.dialogColorChooser = new DialogColorChooser(null);
         init();
     }
 
@@ -174,7 +168,7 @@ public class MarkerEditor extends Container {
 
             private void updateName() {
                 marker.setLabel(nameInput.getText());
-                preview.repaint();
+
             }
         });
         return nameInput;
@@ -191,56 +185,10 @@ public class MarkerEditor extends Container {
         Color selected = this.dialogColorChooser.chooseColor(this.marker.getColor());
         color.setBackground(selected);
         this.marker.setColor(selected);
-        this.preview.repaint();
     }
 
     void changed() {
         this.onUpdate.forEach(Runnable::run);
     }
 
-    private JPanel createValuesPanel() {
-        JPanel valuesPanel = new JPanel(new GridBagLayout());
-        OGridBagConstraints gbc = new OGridBagConstraints();
-        gbc.ipady = 10;
-
-        JLabel[] labels = {new JLabel("X: "), new JLabel("Y: "), new JLabel(getWord("dialog.markers.width")), new JLabel(getWord("dialog.markers.height")),};
-        JSpinner[] spinners = {
-            createSpinner(this.marker::getX, this.marker::setX),
-            createSpinner(this.marker::getY, this.marker::setY),
-            createSpinner(this.marker::getWidth, this.marker::setWidth),
-            createSpinner(this.marker::getHeight, this.marker::setHeight)
-        };
-
-        gbc.fill = OGridBagConstraints.HORIZONTAL;
-        gbc.setSizeAndWeight(1, 1, 0.3, 0);
-
-        int y = 0;
-        for (; y < labels.length; y++) {
-            gbc.gridy = y;
-
-            gbc.gridx = 0;
-            gbc.weightx = 0.3;
-            valuesPanel.add(labels[y], gbc);
-
-            gbc.gridx = 1;
-            gbc.weightx = 0.7;
-            valuesPanel.add(spinners[y], gbc);
-        }
-
-        return valuesPanel;
-    }
-
-    private JSpinner createSpinner(IntSupplier get, IntConsumer set) {
-        SpinnerNumberModel model = new SpinnerNumberModel(get.getAsInt(), 0, Integer.MAX_VALUE, 1);
-        JSpinner spinner = new JSpinner(model);
-
-        spinner.addChangeListener(e -> {
-            set.accept((Integer) spinner.getModel().getValue());
-            this.preview.repaint();
-        });
-
-        this.onUpdate.add(() -> model.setValue(get.getAsInt()));
-
-        return spinner;
-    }
 }
