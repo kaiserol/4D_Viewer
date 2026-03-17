4cfea9226e27a6912a3ec8e4f4720ad1e4cfcbe9
Lion Rexhepi <lrexhepi@uni-koeln.de>
2026-03-17 11:26:20 +0100

    Pull up members

--- src/main/java/de/uzk/markers/interactions/PointMarkerModificator.java
+++ src/main/java/de/uzk/markers/interactions/PointMarkerModificator.java
@@ -1,42 +1,17 @@
 package de.uzk.markers.interactions;
 
-import de.uzk.edit.markers.MoveRotatableEdit;
 import de.uzk.markers.Marker;
 import de.uzk.markers.PointMarker;
 import de.uzk.markers.PointMarkerShape;
 
 import java.awt.*;
-import java.awt.geom.Point2D;
-
-import static de.uzk.Main.workspace;
 
 public class PointMarkerModificator extends RotatableMarkerModificator<PointMarker> {
-    private MoveRotatableEdit edit;
-    public PointMarkerModificator(PointMarker marker) { super(marker); }
+     public PointMarkerModificator(PointMarker marker) { super(marker); }
 
     @Override
     public void handleResize(Point mousePos) {
         // noop
-    }
-
-    @Override
-    public void beginMove() {
-        edit = new MoveRotatableEdit(marker);
-    }
-
-    @Override
-    public void handleMove(Point mousePos) {
-        double newX = mousePos.x + PointMarker.SIZE;
-        double newY = mousePos.y + PointMarker.SIZE;
-        edit.move(newX - marker.getCenter().getX(), newY - marker.getCenter().getY());
-        marker.setCenter(new Point2D.Double(newX, newY));
-    }
-
-    @Override
-    public void finishMove() {
-        if(edit == null) return;
-        workspace.getEditManager().registerEdit(edit);
-        edit = null;
     }
 
     @Override
